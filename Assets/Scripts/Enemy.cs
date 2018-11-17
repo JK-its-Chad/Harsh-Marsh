4dee437674c2eaa2daef7ae799cc8badbc47c2c5
GearedAtFace <37555402+GearedAtFace@users.noreply.github.com>
2018-11-17 14:22:44 -0500

    Character Movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float viewRange = 20f;
+
+    bool targetPlayer = false;
+    GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, Player.transform.position) >= viewRange) targetPlayer = true; ;
+        if (Vector3.Distance(transform.position, Player.transform.position) >= 100) Destroy(gameObject);
+
+        if(targetPlayer)
+        {
+            transform.LookAt(Player.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float score = 0;
+    public int health = 100;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z + Input.GetAxis("Vertical"));
+        }
+    }
+}
