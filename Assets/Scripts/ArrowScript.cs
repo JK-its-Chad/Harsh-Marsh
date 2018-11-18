f1e4dde78602f9bcaaf8f4c97be446a1841662f0
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2018-11-18 10:40:13 -0500

    ArrowShooting on Fire2

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour {
+
+    public int damage = 25;
+    public int speed = 25;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
+    void Update ()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Enemy E = (Enemy)other.gameObject.GetComponent<Enemy>();
+            if (E) E.takeDamage(damage);
+
+            if (other.gameObject.GetComponent<Rigidbody>())
+            {
+                other.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 200);
+            }
+        }
+        if (other.gameObject.tag != "Player")
+        {
+            Destroy(gameObject, .15f);
+        }
+    }
+}
---
+++ Assets/Scripts/BowScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour {
+
+    float fireRate = 0.75f;
+    float fireTimer = 0f;
+
+    float ammoCharge = 2f;
+    float ammoTimer = 0f;
+    public int ammoAmount = 30;
+
+    [SerializeField] GameObject arrow;
+    [SerializeField] Transform arrowOut;
+    GameObject[] children = new GameObject[2];
+
+    // Use this for initialization
+    void Start ()
+    {
+        int i = 0;
+        foreach (MeshRenderer t in GetComponentsInChildren<MeshRenderer>())
+        {
+            children[i] = t.gameObject;
+            children[i].SetActive(false);
+            i++;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        fireTimer -= Time.deltaTime;
+        ammoTimer -= Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
+        {
+            foreach (GameObject gm in children)
+            {
+                gm.SetActive(false);
+            }
+        }
+
+        if (Input.GetButton("Fire2") && fireTimer <= 0 && ammoAmount > 0)
+        {
+            ammoAmount--;
+            fireTimer = fireRate;
+            foreach (GameObject gm in children)
+            {
+                gm.SetActive(true);
+            }
+            Instantiate(arrow, arrowOut.position, transform.rotation);
+            ammoTimer = ammoCharge;
+        }
+
+        if(ammoTimer <= 0 && ammoAmount < 30)
+        {
+            ammoTimer = ammoCharge;
+            ammoAmount++;
+        }
+	}
+}
