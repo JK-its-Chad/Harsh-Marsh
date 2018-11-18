5a18af4dcfc29675d430b537bb1563450337f1cf
Hyperion47 <37087822+Hyperion47@users.noreply.github.com>
2018-11-18 03:06:45 -0500

    Worked on High Score
    I have the high score variable incrementing and decrementing, with just a little work i'll be able to make it store an organized list of scores. I'll use the overflow property in the text field to try and fit a scrolling 100 top scores chart. Then we just link all of the enemies to the current score variable that runs the script and we're good to go

---
+++ Assets/Scripts/HighScoreStuff/ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    public Text HighScoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void IncrementPress()
+    {
+        ScoreManager.HighScore++;
+        ScoreManager.SaveScore();
+        if (ScoreManager.LoadScore())
+        {
+            HighScoreDisplay.text = "High Score: " + ScoreManager.HighScore;
+        }
+    }
+
+    public void DecrementPress()
+    {
+        ScoreManager.HighScore--;
+        ScoreManager.SaveScore();
+        if (ScoreManager.LoadScore())
+        {
+            HighScoreDisplay.text = "High Score: " + ScoreManager.HighScore;
+        }
+    }
+}
