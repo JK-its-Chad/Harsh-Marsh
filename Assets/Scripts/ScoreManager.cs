f0b195d7019b9fc36a4adc2b5eaecd724f3682cb
Hyperion47 <37087822+Hyperion47@users.noreply.github.com>
2018-11-17 23:49:57 -0500

    i dont even remember what i changed
    so...yep

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class ScoreManager {
+
+    private static string _scoreFilePath = Application.persistentDataPath + "/score.binary";
+    private static ScoreHolder score = new ScoreHolder();
+    public static int HighScore
+    {
+        get
+        {
+            return score.HighScore;
+        }
+        set
+        {
+            score.HighScore = value;
+        }
+    }
+
+    public static void SaveScore()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream scoreFile = File.Create(_scoreFilePath);
+
+        formatter.Serialize(scoreFile, score);
+
+        scoreFile.Close();
+    }
+
+    public static bool LoadScore()
+    {
+        if (!File.Exists(_scoreFilePath)) { return false; }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream scoreFile = File.Open(_scoreFilePath, FileMode.Open);
+
+        score = (ScoreHolder)formatter.Deserialize(scoreFile);
+        scoreFile.Close();
+        return true;
+    }
+}
